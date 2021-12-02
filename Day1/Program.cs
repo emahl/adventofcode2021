bb648545bec590be00098d44c8deef94a2e117d2
Emil Ahlqvist <emil.ahlqvist@trimma.se>
2021-12-02 08:24:32 +0100

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,31 @@
+﻿Part1();
+Part2();
+
+void Part1() {
+    var nrOfIncreasingMeasurements = 0;
+    var previousMeasurement = -1;
+    var measurements = File.ReadAllLines("input.txt").Select(l => int.Parse(l));
+    foreach (var measurement in measurements) {
+        if (previousMeasurement > -1 && (measurement > previousMeasurement)) {
+            nrOfIncreasingMeasurements++;
+        }
+        previousMeasurement = measurement;
+    }
+
+    Console.WriteLine("Part 1: {0}", nrOfIncreasingMeasurements);
+}
+
+void Part2() {
+    var nrOfIncreasingMeasurements = 0;
+    var previousSum = -99999;
+    var measurements = File.ReadAllLines("input.txt").Select(l => int.Parse(l));
+    for (var i = 0; i < measurements.Count(); i ++) {
+        var sum = measurements.Skip(i).Take(3).Sum();
+        if (previousSum > -99999 && sum > previousSum) {
+            nrOfIncreasingMeasurements++;
+        }
+        previousSum = sum;
+    }
+
+    Console.WriteLine("Part 2: {0}", nrOfIncreasingMeasurements);
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Advent Of Code 2021
+All the exercises (days) that I code before I get tired of this..
